0fe2f17c87333aceb6261cdda936387bfee65cf1
Hudson Ayers <hayers@stanford.edu>
2018-11-19 15:28:06 -0800

    Add some puffer code so mock server is available for testing, update .gitignore, add empty directories where pensieve requires they exist to run

---
+++ multi_video_sim/mock_server/mock_media_server.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdio.h>
+#include <chrono>
+#include <thread>
+#include <cstdint>
+#include "ipc_socket.hh"
+#include <nlohmann/json.hpp>
+#include <endian.h>
+
+using json = nlohmann::json;
+using namespace std;
+
+string put_field(const uint16_t n)
+{
+  const uint16_t network_order = htobe16(n);
+  return string(reinterpret_cast<const char *>(&network_order),
+                sizeof(network_order));
+}
+
+auto start = std::chrono::system_clock::now();
+
+int main () {
+    // Open IPC Channel to pensieve ABR decision-maker
+    remove("/tmp/pensieve");
+    IPCSocket sock;
+    sock.bind("/tmp/pensieve");
+    sock.listen();
+    auto connection = sock.accept();
+    while(1) {
+
+        // Get info from Pensieve
+        auto read_data = connection.read();
+        if (read_data.empty()) {
+            cout << "Empty read, exiting" << endl;
+            break;
+        }
+        cout << read_data << endl;
+
+        auto end = std::chrono::system_clock::now();
+        double elapsed_seconds = std::chrono::duration_cast<
+                                 std::chrono::duration<double> >(end - start).count();
+        cout << "IPC total time: " << elapsed_seconds << endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2)); //simulate real media server doing other stuff
+        start = std::chrono::system_clock::now();
+        //RESPOND
+        json j;
+        j["delay"] = 3.99182; // ms
+        j["playback_buf"] = 7230; // ms
+        j["rebuf_time"] = 150; // ms
+        j["last_chunk_size"] = 0.1850032; // MB
+        j["next_chunk_sizes"] = {0.181801, 0.450283, 0.668286, 1.034108, 1.728879, 2.354772, 2.83424, 3.1289123}; //MB
+        uint16_t json_len = j.dump().length();
+
+        connection.write(put_field(json_len) + j.dump());
+        //connection.write(to_string(j.dump().length()));
+//        connection.write("bit rate = 5, pbuf = 6");
+    }
+}
