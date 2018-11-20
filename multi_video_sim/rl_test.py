0b825fd0e79ed5983cf11ce6e33e78f5955e9402
Hudson Ayers <hayers@stanford.edu>
2018-11-19 16:13:02 -0800

    Initial changes to make multi-video testing compatible with Puffer

--- multi_video_sim/rl_test.py
+++ multi_video_sim/rl_test.py
@@ -1,20 +1,18 @@
 import os
+import json
 import sys
+import socket
 os.environ['CUDA_VISIBLE_DEVICES']=''
 import numpy as np
 import tensorflow as tf
 import a3c
-import env
-
+import struct
 
 S_INFO = 7  # bit_rate, buffer_size, bandwidth_measurement, measurement_time, chunk_til_video_end
 S_LEN = 10  # take how many frames in the past
 A_DIM = 10
 ACTOR_LR_RATE = 0.0001
 CRITIC_LR_RATE = 0.001
-NUM_AGENTS = 16
-TRAIN_SEQ_LEN = 200  # take as a train batch
-MODEL_SAVE_INTERVAL = 100
 VIDEO_BIT_RATE = [200,300,450,750,1200,1850,2850,4300,6000,8000]  # Kbps
 BUFFER_NORM_FACTOR = 10.0
 M_IN_K = 1000.0
@@ -24,24 +22,39 @@ SMOOTH_PENALTY = 1
 DEFAULT_QUALITY = 1  # default video quality without agent
 RANDOM_SEED = 42
 RAND_RANGE = 1000
-LOG_FILE = './test_results/log_sim_rl'
-TEST_TRACES = './cooked_test_traces/'
-TEST_VIDEO_FOLDER = './test_video/'
-# log in format of time_stamp bit_rate buffer_size rebuffer_time chunk_size download_time reward
 NN_MODEL = sys.argv[1]
+SERVER_ADDRESS = '/tmp/pensieve'
+AVG_AUDIO_SIZE = 64; # Approx 64 Kb of audio sent every 2 seconds, add this to video chunks
 
 
-def action_to_bitrate(action, mask, a_dim=A_DIM):
-    assert len(mask) == a_dim
-    assert action >= 0
-    assert action < a_dim
-    assert mask[action] == 1
-    # index starts at 0, ':' is non-inclusive
-    return np.sum(mask[:action])  
+def start_ipc_client():
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(SERVER_ADDRESS)
+    return sock
+
+
+def get_puffer_info(sock):
+    json_len = sock.recv(2)
+    json_len_num = struct.unpack("!H", json_len)[0]
+    json_data = sock.recv(json_len_num)
+    puffer_info = json.loads(json_data)
+    delay = puffer_info['delay'];
+    playback_buf = puffer_info['playback_buf'];
+    rebuf_time = puffer_info['rebuf_time'];
+    last_chunk_size = puffer_info['last_chunk_size'];
+    next_chunk_size = puffer_info['next_chunk_sizes'];
+
+    return delay, playback_buf, rebuf_time, last_chunk_size, next_chunk_size
+
+
+def send_puffer_next_action(sock, bit_rate):
+    err = sock.sendall(str(bit_rate))
+    return err
+
 
 def bitrate_to_action(bitrate, mask, a_dim=A_DIM):
     assert len(mask) == a_dim
-    assert bitrate >= 0 
+    assert bitrate >= 0
     assert bitrate < np.sum(mask)
     cumsum_mask = np.cumsum(mask) - 1
     action = np.where(cumsum_mask == bitrate)[0][0]
@@ -54,13 +67,8 @@ def main():
 
     assert len(VIDEO_BIT_RATE) == A_DIM
 
-    net_env = env.Environment(fixed_env=True, 
-                              trace_folder=TEST_TRACES, 
-                              video_folder=TEST_VIDEO_FOLDER)
-
-    log_path = LOG_FILE + '_' + net_env.all_file_names[net_env.trace_idx]
-    log_file = open(log_path, 'wb')
-
+    # Originally defined in env.py
+    mask = [1,1,1,1,1,1,1,0,1,0] # TODO: Make me a variable dependent on channel
     with tf.Session() as sess:
 
         actor = a3c.ActorNetwork(sess,
@@ -79,31 +87,32 @@ def main():
             saver.restore(sess, NN_MODEL)
             print("Testing model restored.")
 
-        time_stamp = 0
-
         last_bit_rate = DEFAULT_QUALITY
         bit_rate = DEFAULT_QUALITY
 
-        action = bitrate_to_action(bit_rate, net_env.video_masks[net_env.video_idx])
+        action = bitrate_to_action(bit_rate, mask)
         last_action = action
 
         s_batch = [np.zeros((S_INFO, S_LEN))]
 
         entropy_record = []
 
-        video_count = 0
+        video_chunks_sent = 0;
+        video_num_chunks = 43200; # 24 hours of video. Is this an acceptable proxy for never ending video?
+
+        puffer_sock = start_ipc_client()
+        puffer_sock.sendall("Pensieve ready")
 
         while True:  # serve video forever
             # the action is from the last decision
             # this is to make the framework similar to the real
-            delay, sleep_time, buffer_size, \
-                rebuf, video_chunk_size, end_of_video, \
-                video_chunk_remain, video_num_chunks, \
-                next_video_chunk_size, mask = \
-                net_env.get_video_chunk(bit_rate)
 
-            time_stamp += delay  # in ms
-            time_stamp += sleep_time  # in ms
+            video_chunk_remain = video_num_chunks - video_chunks_sent;
+
+            delay, buffer_size, \
+                rebuf, video_chunk_size, \
+                next_video_chunk_size = \
+                get_puffer_info(puffer_sock)
 
             reward = VIDEO_BIT_RATE[action] / M_IN_K \
                      - REBUF_PENALTY * rebuf \
@@ -112,16 +121,6 @@ def main():
 
             last_bit_rate = bit_rate
             last_action = action
-
-            # log time_stamp, bit_rate, buffer_size, reward
-            log_file.write(str(time_stamp / M_IN_K) + '\t' +
-                           str(VIDEO_BIT_RATE[action]) + '\t' +
-                           str(buffer_size) + '\t' +
-                           str(rebuf) + '\t' +
-                           str(video_chunk_size) + '\t' +
-                           str(delay) + '\t' +
-                           str(reward) + '\n')
-            log_file.flush()
 
             # retrieve previous state
             if len(s_batch) == 0:
@@ -135,7 +134,7 @@ def main():
             # this should be S_INFO number of terms
             state[0, -1] = VIDEO_BIT_RATE[action] / float(np.max(VIDEO_BIT_RATE))  # last quality
             state[1, -1] = buffer_size / BUFFER_NORM_FACTOR
-            state[2, -1] = float(video_chunk_size) / float(delay) / M_IN_K  # kilo byte / ms
+            state[2, -1] = float(video_chunk_size) / float(delay) / M_IN_K  # kilo byte / ms # This is really just throughput
             state[3, -1] = float(delay) / M_IN_K
             state[4, -1] = video_chunk_remain / float(video_num_chunks)
             state[5, :] = -1
@@ -158,32 +157,12 @@ def main():
             # because there is an intrinsic discrepancy in passing single state and batch states
             action = bitrate_to_action(bit_rate, mask)
 
+            # Now I have my action! Send this action back to the Puffer server over IPC
+            send_puffer_next_action(puffer_sock, bit_rate)
+
             s_batch.append(state)
 
             entropy_record.append(a3c.compute_entropy(action_prob[0]))
-
-            if end_of_video:
-                log_file.write('\n')
-                log_file.close()
-
-                del s_batch[:]
-
-                last_bit_rate = DEFAULT_QUALITY
-                bit_rate = DEFAULT_QUALITY  # use the default action here
-
-                action = bitrate_to_action(bit_rate, mask)
-                last_action = action
-
-                s_batch.append(np.zeros((S_INFO, S_LEN)))
-                entropy_record = []
-
-                video_count += 1
-
-                if video_count >= len(net_env.all_cooked_bw):
-                    break
-
-                log_path = LOG_FILE + '_' + net_env.all_file_names[net_env.trace_idx]
-                log_file = open(log_path, 'wb')
 
 
 if __name__ == '__main__':
