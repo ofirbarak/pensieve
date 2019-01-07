31b90eea08e65f5a2c097c37f6e678769f325720
Hudson Ayers <hayers@stanford.edu>
2019-01-07 13:50:25 -0800

    Move setup.py to shell script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# tensorflow
+sudo pip install tensorflow==1.1.0
+
+# tflearn
+sudo pip install tflearn==0.3.1
+sudo apt-get -y install python-h5py
+sudo apt-get -y install python-scipy
