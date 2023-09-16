8255ea06991db1c1ae14deb98c0c511ddd74a9f2
Ofir Barak <ofirbirka@gmail.com>
2023-09-16 16:16:38 +0300

    setup: Add scripts for automate development setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# python2
+# python3
 sudo apt install python-pip
 python2 -m pip install virtualenv
 python2 -m virtualenv venv
 source venv/bin/activate
+
+# requirements for selenium
+sudo apt-get install python-setuptools
+sudo apt install xvfb
 
 pip install -r requirements.txt
 
@@ -12,3 +16,13 @@ pip install -r requirements.txt
 sudo apt-get -y install python-h5py
 sudo apt-get -y install python-scipy
 sudo apt install python-tk
+
+# apache
+sudo apt install apache2
+sudo ufw allow 'Apache'
+sudo ufw enable
+sudo mkdir -p /var/www/html
+sudo cp -r video_server/* /var/www/html
+echo hostname -I
+
+# go to http://<local-ip>/myindex_BB.html
